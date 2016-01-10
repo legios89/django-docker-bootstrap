64032387e14d98a7d1e03228acb973a36bdd3b55
Egyed Zoltán <zoltan.egyed@vertis.com>
2016-01-10 21:01:30 +0100

    working nginx-uwsgi

--- config/django/run.py
+++ config/django/run.py
@@ -93,6 +93,10 @@ def start_runserver():
 @run.command()
 def start_uwsgi():
     """Starts the service."""
+    ensure_dir('/data/sock',
+               owner='django',
+               group='django',
+               permsission_str='777')
     start = ["uwsgi", "--ini", UWSGI_CONF]
     run_daemon(start, signal_to_send=signal.SIGQUIT, user='django',
                waitfunc=waitfordb, initfunc=init)
