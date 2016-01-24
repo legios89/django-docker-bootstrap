aead2f030827d1b7897a0bc5ba388e9ea7e11883
Egyed Zoltán <zoltan.egyed@vertis.com>
2016-01-24 15:04:43 +0100

    docker file & build process now use the cookiecutter

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,31 @@
+import os
+from random import SystemRandom
+
+
+def generate_secret_key():
+    """
+    Returns a securely generated random string.
+    """
+    allowed_chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#%^&*(-_=+)'
+    return ''.join(SystemRandom().choice(allowed_chars) for i in range(50))
+
+
+def make_secret_key(project_directory):
+    """Generates and saves random secret key"""
+    # Determine the setting_file_location
+    setting = os.path.join(project_directory, 'src/core/settings.py')
+
+    with open(setting) as f:
+        file_ = f.read()
+
+    # Replace "CHANGEME!!!" with SECRET_KEY
+    file_ = file_.replace('{SECRET_KEY}', generate_secret_key(), 1)
+
+    # Write the results to the file
+    with open(setting, 'w') as f:
+        f.write(file_)
+
+
+# 1. Generates and saves random secret key
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+make_secret_key(PROJECT_DIRECTORY)
--- {{cookiecutter.repo_name}}/buildall.sh
+++ {{cookiecutter.repo_name}}/buildall.sh
@@ -1,28 +1,23 @@
 #! /bin/bash
-
 set -e
-
-dir="$(dirname "$BASH_SOURCE")/docker"
+dir="{{cookiecutter.repo_name}}/docker"
 
 echo "base"
 echo "--------------------"
-docker build -t base "$dir/base"
+docker build -t {{cookiecutter.repo_name}}-base "$dir/base"
 
 echo "postgres"
 echo "--------------------"
-docker build -t postgres "$dir/postgres"
+docker build -t {{cookiecutter.repo_name}}-postgres "$dir/postgres"
 
 echo "nginx"
 echo "--------------------"
-docker build -t nginx "$dir/nginx"
+docker build -t {{cookiecutter.repo_name}}-nginx "$dir/nginx"
 
 echo "django-python3"
 echo "--------------------"
-docker build -t django-python3 "$dir/django-python3"
+docker build -t {{cookiecutter.repo_name}}-django-python3 "$dir/django-python3"
 
 echo 'Start the data container'
 echo "--------------------"
 docker-compose --file data-docker-compose.yml up -d
-
-echo 'Please check the dev-docker-compose.yml & docker-compose.yml file!'
-echo 'The proxy service volumes_from should be the created data container.'
