23cb158dad86c4c227c8910547a5542ea05ced3f
Egyed Zoltán <zoltan.egyed@vertis.com>
2016-03-15 15:24:07 +0100

    django restframwroek + react router update

--- {{cookiecutter.repo_name}}/react/components/router.jsx
+++ {{cookiecutter.repo_name}}/react/components/router.jsx
@@ -4,7 +4,8 @@ var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var createHashHistory = require('history/lib/createHashHistory');
+var useRouterHistory = ReactRouter.useRouterHistory;
+var createHashHistory = require('react-router/node_modules/history/lib/createHashHistory');
 
 // Components
 var App = require('./app.jsx');
@@ -17,12 +18,12 @@ var NotFoundRoute = React.createClass({
   }
 });
 
-var history = createHashHistory({queryKey: false});
+var appHistory = useRouterHistory(createHashHistory)({queryKey: false});
 var mainContainer = document.getElementById('app-place');
 var language = mainContainer.attributes['data-language'].value;
 
 ReactDOM.render((
-  <Router history={history}>
+  <Router history={appHistory}>
     <Route component={App} language={language}>
       <Route path="/" component={Home}/>
       <Route path="*" component={NotFoundRoute}/>
